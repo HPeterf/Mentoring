f2eef35c9182ccb059f26b505a0541af80d481fe
HPeter <horvathpeterf@hotmail.com>
2020-01-22 19:07:55 +0100

    security working

--- src/main/java/com/epam/firstaid/entity/Employee.java
+++ src/main/java/com/epam/firstaid/entity/Employee.java
@@ -7,6 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,56 +27,51 @@ import lombok.ToString;
 @NoArgsConstructor
 public class Employee implements Serializable {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
 
-  @Column
-//  @NotBlank(message = "Please add the Name of the Employee")
-  private String name;
+	@Column
+	@NotBlank(message = "Please add the Name of the Employee")
+	private String name;
 
-  @Column
-//  @NotBlank(message = "Please add a location")
-  private String location;
-  
-  
+	@Column
+	@NotBlank(message = "Please add a location")
+	private String location;
 
+	public Employee() {
+		super();
+	}
 
+	public Employee(Long id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
 
-public Employee() {
-	super();
-}
+	public Long getId() {
+		return id;
+	}
 
-public Employee(Long id, String name, String location) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.location = location;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public Long getId() {
-	return id;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setId(Long id) {
-	this.id = id;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public String getName() {
-	return name;
-}
+	public String getLocation() {
+		return location;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-public String getLocation() {
-	return location;
-}
-
-public void setLocation(String location) {
-	this.location = location;
-}
-  
-  
 }
